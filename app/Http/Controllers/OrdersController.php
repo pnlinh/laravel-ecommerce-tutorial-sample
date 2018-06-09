c417be998dd7c54ca62a84c7c1af9a3e9fee2a8a
leo108 <leo108@qq.com>
2018-06-09 08:56:04 +0800

    确认收货

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
@@ -83,6 +84,22 @@ class OrdersController extends Controller
         }
         $request->session()->put('cart', $cart);
         $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
+
+        return $order;
+    }
+
+    public function received(Order $order, Request $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+
+        // 判断订单的发货状态是否为已发货
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+
+        // 更新发货状态为已收到
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
 
         return $order;
     }
